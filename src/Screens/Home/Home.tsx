2ba281cc1721390ac4a9858332f6c154ba26f870
Mohammed Saed <mohd.a.saed@gmail.com>
2024-06-23 23:16:29 +0300

    Update profile image when logged in

--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import { useUser } from '@clerk/clerk-expo';
+import React, { useEffect } from 'react';
 import { Text, View } from 'react-native';
+import { supabase } from '../../Utils/SupabaseConfig';
 
 function Home() {
+	const { user } = useUser();
+
+	const updateAvatar = async () => {
+		await supabase
+			.from('users')
+			.update({ avatar_url: user?.imageUrl })
+			.eq('email', user?.primaryEmailAddress?.emailAddress ?? '')
+			.is('avatar_url', null)
+			.select();
+	};
+
+	useEffect(() => {
+		user && updateAvatar();
+	}, [user]);
+
 	return (
 		<View>
 			<Text>Home Screen</Text>
--- src/Utils/SupabaseConfig.ts
+++ src/Utils/SupabaseConfig.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
+import type { Database } from '../../types/supabase';
 
 const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const SUPABASE_KEY = process.env.EXPO_PUBLIC_SUPABASE_API_KEY!;
 
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_KEY);
